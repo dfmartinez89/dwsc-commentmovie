a315daa2838377db46d39abbe6a89b55e9f99115
Damian Ferro <dfmartinez8909@gmail.com>
2022-05-15 20:42:04 +0200

    first commit

---
+++ src/main/java/dwsc/proyecto/commentmovie/CommentMovieApplication.java
@@ -0,0 +1,13 @@
+package dwsc.proyecto.commentmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentMovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dwsc/proyecto/commentmovie/CommentMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package dwsc.proyecto.commentmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
