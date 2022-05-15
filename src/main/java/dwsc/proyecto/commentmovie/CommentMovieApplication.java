985012979bfef4c73bac364a56743018f7fb35f8
Damian Ferro <dfmartinez8909@gmail.com>
2022-05-15 20:47:36 +0200

    props

--- src/main/java/dwsc/proyecto/commentmovie/CommentMovieApplication.java
+++ src/main/java/dwsc/proyecto/commentmovie/CommentMovieApplication.java
@@ -2,8 +2,10 @@ package dwsc.proyecto.commentmovie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CommentMovieApplication {
 
 	public static void main(String[] args) {
